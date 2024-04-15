edb30353a577cd0af5e99aad45924eaa9e82c932
Nicholas Shapovalov <nickshapovcomp@gmail.com>
2024-04-16 00:12:19 +0100

    refactor: remove menu icon from navbar

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -5,7 +5,6 @@ import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
 import Avatar from '@mui/material/Avatar';
 import Tooltip from '@mui/material/Tooltip';
@@ -69,7 +68,6 @@ function ResponsiveNavbar() {
               onClick={handleOpenNavMenu}
               color="inherit"
             >
-              <MenuIcon />
             </IconButton>
             <Menu
               id="menu-appbar"
